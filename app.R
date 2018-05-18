4054c6268757602e4d7c2a60328fd378d66c7558
nikuya3 <nikuya@dummy.mail>
2018-05-18 14:20:38 +0200

    Finished clickable shiny map

--- app.R
+++ app.R
@@ -1,58 +1,92 @@
-library(shiny)
-library(tidykml)
-library(ggplot2)
-library(magrittr)
+packages = c("shiny", "ggplot2", "tidykml", "magrittr", "sp")
 
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    if (p == "tidykml") {
+      install.packages("devtools")
+      devtools::install_github("briatte/tidykml")
+    }
+    install.packages(p)
+  }
+  
+  library(p, character.only = TRUE)
+}
+
+invisible(sapply(packages, install_if_missing))
 
 mappings <- read.csv("data/mappings.csv", header = FALSE)
+polys <- kml_polygons("data/bezirke.kml")
+data <- read.csv2("data/vie_101.csv", skip = 1)
+# clean dataset
+data <-
+  data[, !(names(data) %in% c("NUTS1", "NUTS2", "NUTS3", "SUB_DISTRICT_CODE"))]  # remove artificial identifiers
+data$REF_DATE <- as.character(data$REF_DATE)
+data$REF_DATE <- as.Date(data$REF_DATE, format = "%Y%m%d")
+data$DISTRICT_CODE <-
+  as.factor((data$DISTRICT_CODE - 90000) / 100)  # convert District codes (90100) to categorical district numbers (1)
+
 to_name <- function(n) {
-  n <- mappings[1][mappings[2] == n,]
+  n <- mappings[1][mappings[2] == n, ]
   paste("BEZIRKSGRENZEOGD.", 8000 + n, sep = "")
 }
+
 from_name <- function(n) {
   num <- gsub("BEZIRKSGRENZEOGD.", "", n)
   num <- as.numeric(num)
   num <- num - 8000
-  mappings[2][num,]
+  mappings[2][num, ]
 }
-polys <- kml_polygons("data/bezirke.kml")
+
+corr <- function (n, m) {
+  cor(data[data$DISTRICT_CODE == n, ]$POP_TOTAL, data[data$DISTRICT_CODE == m, ]$POP_TOTAL)
+}
 
 ui <- fluidPage(
-   titlePanel("Correlation of population change between districts"),
-   sidebarLayout(
-      sidebarPanel(
-         numericInput(inputId = "dist",
-                      label = "District",
-                      min = 1,
-                      max = 23,
-                      value = 1)
-      ),
-      mainPanel(
-         plotOutput("corrsp")
-      )
-   )
+  titlePanel("Correlation of population change between districts (1869--2015)"),
+  plotOutput("corrsp", click = "dist_click")
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-   output$corrsp <- renderPlot({
-     data <- read.csv2("data/vie_101.csv", skip = 1)
-     # clean dataset
-     data <- data[,!(names(data) %in% c("NUTS1", "NUTS2", "NUTS3", "SUB_DISTRICT_CODE"))]  # remove artificial identifiers
-     data$REF_DATE <- as.character(data$REF_DATE)
-     data$REF_DATE <- as.Date(data$REF_DATE, format = "%Y%m%d")
-     data$DISTRICT_CODE <- as.factor((data$DISTRICT_CODE - 90000) / 100)  # convert District codes (90100) to categorical district numbers (1)
-     # calculate correlation of pop totals per district
-     corr <- function (n, m) {
-       cor(data[data$DISTRICT_CODE == n,]$POP_TOTAL, data[data$DISTRICT_CODE == m,]$POP_TOTAL)
-     }
-     corrs <- sapply(1:23, function (n) { sapply(1:23, function (m) { corr(n, m) }) })
-     polys[polys$name != to_name(input$dist),] %>%
-       ggplot(aes(longitude, latitude, group = name, fill = corrs[input$dist,from_name(name)])) +
-       geom_polygon(color = "white")
-   })
+  dist <- 1
+  makeReactiveBinding('dist')
+  output$corrsp <- renderPlot({
+    # calculate correlation of pop totals per district
+    corrs <- sapply(1:23, function (m) {
+      corr(dist, m)
+    })
+    corrs[dist] <- NA
+    polys %>%
+      ggplot(aes(longitude, latitude, group = name, fill = corrs[from_name(name)])) +
+      geom_polygon(color = "white") +
+      theme_void() +
+      guides(fill = guide_legend(paste(
+        "Correlation to district", formatC(dist, width = 2, flag = "0")
+      ), alpha = FALSE)) +
+      scale_fill_gradient2(
+        low = "#822a4d",
+        mid = "white",
+        high = "#2a5486",
+        breaks = c(-1, -0.5, 0, 0.5, 1),
+        limits = c(-1, 1)
+      )
+  })
+
+  observeEvent(input$dist_click, {
+    sad <- lapply(unique(polys$name), function (b) {
+      poly.x <- polys[polys$name == b, ]$longitude
+      poly.y <- polys[polys$name == b, ]$latitude
+      if (point.in.polygon(input$dist_click$x, input$dist_click$y, poly.x, poly.y)) {
+        from_name(b)
+      }
+      else {
+        0
+      }
+    })
+    if (length(unique(sad)) > 1) {
+      dist <<- as.numeric(sad[which(sad != 0)])
+    }
+  })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
-
