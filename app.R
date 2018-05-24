b66b5cc837b0270fef1fb9b0bb52145bd83fe308
nikuya3 <nikuya@dummy.mail>
2018-05-24 17:22:08 +0200

    Added population plots to shiny app

--- app.R
+++ app.R
@@ -31,10 +31,31 @@ corr <- function (n, m) {
   cor(data[data$DISTRICT_CODE == n, ]$POP_TOTAL, data[data$DISTRICT_CODE == m, ]$POP_TOTAL)
 }
 
+plot.pop <- function(district) {
+  data <- data[data$DISTRICT_CODE == district,]
+  title <- paste("Population in ", district, ". district per year", sep = "")
+  #plot(POP_TOTAL ~ REF_DATE, data, main = title, xlab = "Date", ylab = "Population")
+  #axis.Date(1, at = c(seq(min(data$REF_DATE), max(data$REF_DATE), by = "50 years"), max(data$REF_DATE)))
+  ggplot(data = data, mapping = aes(REF_DATE, POP_TOTAL)) +
+    labs(title = title, x = "Date", y = "Population") +
+    scale_x_date(date_breaks = "15 years", date_labels = "%Y") +
+    scale_y_continuous(limits = c(0, 200000)) +
+    geom_point() +
+    geom_line()
+}
+
 ui <- fluidPage(
   fluidRow(
     column(2),
-    column(8, h1("Correlation of population change between districts (1869--2015)")),
+    column(8,
+           h1("Visualizing population growth of Vienna between 1869 and 2015", style = "text-align: center"),
+           p("An analysis of population growth of Viennese districts in the last two centuries. The first interactive graph shows the correlation of population growth between districts. The second graph details the population growth for each individual district.", style = "text-align: center")
+           ),
+    column(2)
+  ),
+  fluidRow(
+    column(2),
+    column(8, h2("Correlation of population change between districts", style = "text-align: center")),
     column(2)
   ),
   fluidRow(
@@ -48,8 +69,39 @@ ui <- fluidPage(
     column(2),
     column(8,
            wellPanel(
-             p("Click on district to display correlations"),
-             a("Source: https://www.data.gv.at/katalog/dataset/091a085f-2652-429f-8dde-c69199440ddf (Stadt Wien)", href = "https://www.data.gv.at/katalog/dataset/091a085f-2652-429f-8dde-c69199440ddf")
+             p("Click on a district in the map to display population growth correlations.")
+          )
+    ),
+    column(2)
+  ),
+  fluidRow(
+    column(2),
+    column(8, h2("Population by district", style = "text-align: center")),
+    column(2)
+  ),
+  fluidRow(
+    column(2),
+    column(8,
+      sidebarLayout(
+        sidebarPanel(
+            numericInput("growth_district", "District", 1, min = 1, max = 23, step = 1)
+        ),
+        mainPanel(
+          plotOutput("growth")
+        )
+      )
+    ),
+    column(2)
+  ),
+  fluidRow(
+    column(2),
+    column(8,
+           wellPanel(
+             div(
+               span("Source: "),
+               a("https://www.data.gv.at/katalog/dataset/091a085f-2652-429f-8dde-c69199440ddf", href = "https://www.data.gv.at/katalog/dataset/091a085f-2652-429f-8dde-c69199440ddf"),
+               span(" (Stadt Wien)")
+             )
            )
     ),
     column(2)
@@ -79,6 +131,10 @@ server <- function(input, output) {
         breaks = c(-1, -0.5, 0, 0.5, 1),
         limits = c(-1, 1)
       )
+  })
+
+  output$growth <- renderPlot({
+    plot.pop(input$growth_district)
   })
 
   observeEvent(input$dist_click, {
